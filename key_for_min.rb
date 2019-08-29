a8b725ce12b8232ac0cea3d41a8a1d5545b80f8d
Kierra M <k.merriman32@hotmail.com>
2019-08-29 21:11:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,15 @@
 
 def key_for_min_value(hash)
   lowest_key = nil
-  lowest_value = Float::INFINITY
-  hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
+  lowest_value = Float::INFINITY 
+  hash.each do |key, value|
+    if value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
   end
   lowest_key
 end
+
+
+#(Float:: INFINITY) Its a good default lowest_value because you can be sure that any other number is less than infinityso lowest_key always gets replaced in line 10 the first time.#float :: INFINITY is a constant that represents infinity
